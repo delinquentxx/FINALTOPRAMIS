524cd3449f711d7d7c5ea3ceb74e626e7518f01b
meilyvillaverde <mdvillaverde1@up.edu.ph>
2022-03-29 16:26:41 +0800

    added tabs

--- app.py
+++ app.py
@@ -4,5 +4,29 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'LIS161'
 
 @app.route('/', methods=["GET"])
-def index():
+def home():
     return render_template("home.html")
+
+@app.route('/partnerships', methods=["GET"])
+def partnerships():
+    return render_template("patnerships.html")
+
+@app.route('/minutes', methods=["GET"])
+def minutes():
+    return render_template("minutes.html")
+
+@app.route('/announcements', methods=["GET"])
+def announcements():
+    return render_template("announcements.html")
+
+@app.route('/registration', methods=["GET", "POST"])
+def registration():
+    return render_template("registration.html")
+
+@app.route('/gallery', methods=["GET", "POST"])
+def gallery():
+    return render_template("gallery.html")
+
+@app.route('/calendar', methods=["GET"])
+def calendar():
+    return render_template("calendar.html")
